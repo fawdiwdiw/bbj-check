3df4ec29bc5b4cf1e1fdef62d56ee405a882ffe3
fawdiwdiw <fandyardiw@gmail.com>
2026-04-26 01:51:08 +0700

    Update app.py

--- app.py
+++ app.py
@@ -266,8 +266,14 @@ if st.session_state.load_dinas:
                 nilai_bbj = data_8102[data_8102["kode"]=="810299999999"]["saldo"].sum()
                 
                 if nilai_bbj != 0:
-                    st.error("❌ Masih ada BBJ BLUD (810299999999) senilai Rp {format_rupiah(nilai_bbj)}, Silahkan revisi dahulu!")
-                    st.stop()
+                st.error(f"""
+                ❌ Masih ada BBJ BLUD!
+                
+                Nilai 810299999999: Rp {format_rupiah(nilai_bbj)}
+                
+                Silakan revisi terlebih dahulu.
+                """)
+                st.stop()
                 
                 if st.button("💾 Simpan SIAP ke Database"):
 
